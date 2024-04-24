ac5df35fab1978e1b91a7994ba161d65d4a74bef
Dmitry Naumov <komrad.nda@yandex.ru>
2024-04-24 22:46:09 +0300

    продолжаю изучать тесты

--- src/01-hello-tests/01.test.ts
+++ src/01-hello-tests/01.test.ts
@@ -1,16 +1,51 @@
-import {sum} from './01';
+import { mult, sum, splitIntoWords } from './01'
 
 test('sum should be correct', () => {
+	// * подготовительные данные (data)
+	const a = 1
+	const b = 2
+	const c = 3
+
+	// * действие (action)
+	const result1 = sum(a, b)
+	const result2 = sum(b, c)
+
+	// 	* ожидаем результат (expect result)
+	expect(result1).toBe(3)
+	expect(result2).toBe(5)
+})
+
+test('multiplay should be correct', () => {
 	// * подготовительные данные
 	const a = 1
 	const b = 2
 	const c = 3
 
 	// * действие
-	const result1 = sum(a, b)
-	const result2 = sum(b, c)
+	const result1 = mult(a, b)
+	const result2 = mult(b, c)
 
 	// 	* ожидаем результат
-	expect(result1).toBe(3)
-	expect(result2).toBe(5)
+	expect(result1).toBe(2)
+	expect(result2).toBe(6)
+})
+
+test('spliting into should be correct', () => {
+	const sent1 = 'Hello my friend!'
+	const sent2 = 'JS - the best  programming language.'
+
+	const result1 = splitIntoWords(sent1)
+	const result2 = splitIntoWords(sent2)
+
+	expect(result1.length).toBe(3)
+	expect(result1[0]).toBe('hello')
+	expect(result1[1]).toBe('my')
+	expect(result1[2]).toBe('friend')
+
+	expect(result2.length).toBe(5)
+	expect(result1[0]).toBe('JS')
+	expect(result1[1]).toBe('the')
+	expect(result1[2]).toBe('best')
+	expect(result1[3]).toBe('programming')
+	expect(result1[4]).toBe('language')
 })
--- src/01-hello-tests/01.ts
+++ src/01-hello-tests/01.ts
@@ -8,8 +8,7 @@ export function mult(a: number, b: number) {
 	return a * b
 }
 
-// function splitIntoWords(sentense: string) {
-// 	return [
-// 		'hello', 'my', 'friends'
-// 	]
-// }
+export function splitIntoWords(sentense: string) {
+	const words = sentense.toLowerCase().split(' ')
+	return words.filter(w => w !== '').map(w => w.replace('!', ''))
+}
